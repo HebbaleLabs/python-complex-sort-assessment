5f9ec6da66b05b18b590b80becc96027131c5bcc
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-03-10 08:31:18 +0530

    Added instructions and main method.

--- sortstudents.py
+++ sortstudents.py
@@ -20,4 +20,19 @@ class Student:
     return "An instance of Student: [ %a, %a, %f ]" % (self.sid ,self.name , self.cgpa)
 
 def sort(studentlist):
+  #
+  # IMPORTANT:
+  # 1. Write your solution in this function
+  # 2. Do not make changes to the function signature
+  # 3. Use the 'pip-install' command in the IDE, to install any requirements. This takes a few seconds.
+  # 4. Use the 'run' command in the IDE, to run the main method. You can also invoke the main method from the terminal.
+  # 5. Use the 'run-tests' command in the IDE, to run unit tests. The 'pip-install' command must be run before this.
   return None
+
+def main():
+  example_list_of_students = [ Student(sid = 1, name= 'A', cgpa = 2.5), Student(sid = 2, name = 'B', cgpa= 3.5)]
+  sorted_students = sort(example_list_of_students)
+  print('Sorted list: {0}\n'.format(sorted_students))
+
+if __name__ == '__main__':
+  main()
